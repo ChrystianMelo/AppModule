b40d50912f13e3e4e99dcd1fde653bcc58a31aba
Chrystian Melo <31261075+ChrystianMelo@users.noreply.github.com>
2020-05-30 06:56:18 -0300

    First commit

---
+++ app/src/main/java/com/example/appmodule/SplashScreen.java
@@ -0,0 +1,22 @@
+package com.example.appmodule;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_home);
+            changeScreen();
+        }
+
+        public void changeScreen() {
+            Intent intent = new Intent(this, HomeActivity.class);
+            startActivity(intent);
+            finish();
+        }
+}
