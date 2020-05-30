253dfc2838fd03c3f078e1662b64ca38bc0aa05f
Chrystian Melo <31261075+ChrystianMelo@users.noreply.github.com>
2020-05-30 04:07:06 -0300

    Splash -> Login -> Home

--- app/src/main/java/com/example/appmodule/LoginActivity.java
+++ app/src/main/java/com/example/appmodule/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.example.appmodule;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,17 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button btnNext = findViewById(R.id.login_btn_next);
+
+        final Intent intent = new Intent(this, HomeActivity.class);
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
