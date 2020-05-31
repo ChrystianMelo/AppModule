8e1d6a0897fd21ea6dd07cb7eeb05d3d1be89470
Chrystian Melo <31261075+ChrystianMelo@users.noreply.github.com>
2020-05-31 02:39:32 -0300

    Feed w/ RecyclerView

---
+++ app/src/main/java/com/example/appmodule/FeedAdapter.java
@@ -0,0 +1,57 @@
+package com.example.appmodule;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class FeedAdapter extends RecyclerView.Adapter<FeedAdapter.MyViewHolder> {
+    Context ct;
+    String[] title;
+    String[] content;
+    int[] images;
+
+    public FeedAdapter(Context ct, String[] title, String[] content, int[] images){
+        this.ct = ct;
+        this.title = title;
+        this.content = content;
+        this.images = images;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(this.ct);
+        View view = inflater.inflate(R.layout.feed_adapter,parent,false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.v1.setText(this.title[position]);
+        holder.v2.setText(this.content[position]);
+        holder.img.setImageResource(this.images[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return 2;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView v1,v2;
+        ImageView img;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            v1 = itemView.findViewById(R.id.txt1);
+            v2 = itemView.findViewById(R.id.txt2);
+            img = itemView.findViewById(R.id.image);
+        }
+    }
+}
--- app/src/main/java/com/example/appmodule/HomeActivity.java
+++ app/src/main/java/com/example/appmodule/HomeActivity.java
@@ -1,14 +1,37 @@
 package com.example.appmodule;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class HomeActivity extends AppCompatActivity {
 
+    String[] title;
+    String[] content;
+    int images[] = {R.drawable.logo,R.drawable.logo};
+    RecyclerView view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        view = findViewById(R.id.rv_home_view);
+
+        title = getCount(title,2);
+        content = getCount(title,2);
+
+        FeedAdapter adapter = new FeedAdapter(this, title,content,images);
+        view.setAdapter(adapter);
+        view.setLayoutManager(new LinearLayoutManager(this));
+
+    }
+
+    String[] getCount(String[] str, int size){
+        str = new String[size];
+        for(int i =0; i<size;i++)
+            str[i] = "info"+Integer.toString(i);
+        return str;
     }
 }
