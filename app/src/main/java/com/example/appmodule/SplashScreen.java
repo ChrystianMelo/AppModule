2e7ab8b6ad52e357c41a231beaa6c6fd920e8a6b
Chrystian Melo <31261075+ChrystianMelo@users.noreply.github.com>
2020-06-02 18:35:40 -0300

    Profile Session

--- app/src/main/java/com/example/appmodule/SplashScreen.java
+++ app/src/main/java/com/example/appmodule/SplashScreen.java
@@ -5,18 +5,27 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class SplashScreen extends AppCompatActivity {
 
-        @Override
+    private FirebaseAuth mAuth;
+
+    @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_home);
+            
+            mAuth = FirebaseAuth.getInstance();
+            
             changeScreen();
         }
 
         public void changeScreen() {
-            Intent intent = new Intent(this, LoginActivity.class);
-            startActivity(intent);
+            FirebaseUser user = mAuth.getCurrentUser();
+            if(user == null) startActivity(new Intent(this, LoginActivity.class));
+            else             startActivity(new Intent(this, HomeActivity.class));
             finish();
         }
 }
